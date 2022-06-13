0ff7772ddfc69a8cb43afe53fed9b33da0924363
matiasfvs <49506146+matiasfvs@users.noreply.github.com>
2022-06-13 17:39:41 -0400

    commit
     estilos

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -19,11 +19,14 @@ const useStyles = makeStyles({
   },
 })
 
-
 const HomeScreen =({data,zonas,getZonas})=>{
 
   const [semana, setSemana] = React.useState('');
   const classes = useStyles();
+
+  const handleLogOut = async () => {
+    console.log('cerrar sesion')
+  }
 
   const handleChange = (event) => {
     setSemana(event.target.value);
@@ -65,7 +68,11 @@ const HomeScreen =({data,zonas,getZonas})=>{
           <div className ='bannerMenu'>
               <div className='comboStyle'>Seleccione una Semana :  {llenaCombo()}</div>
               <div className='logoStyle'>DIV LOGO</div>
-              <div className='loginOff'>DIV DESLOGIN</div>
+              <div className='loginOff'>
+              <text>Cerrar Sesión</text> 
+                <a href="/" onClick={()=>handleLogOut()} color="white">
+                <svg xmlns="http://www.w3.org/2000/svg" background-color="#FFFFF" height="24" viewBox="0 0 24 24" width="24" color="#FFFFF"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z"/></svg>
+              </a></div>
           </div>
 
           <div>
